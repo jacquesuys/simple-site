898ba21f7ffb49e448275fb978ff20481866a567
Jacques Uys <jaxuys@gmail.com>
2016-07-09 16:20:20 +0200

    Back to top, Footer & Blog

--- public/js/main.js
+++ public/js/main.js
@@ -33,8 +33,45 @@ function hero() {
   document.querySelector('body').classList.add('hero--visible');
 }
 
+function backToTop() {
+  var body = document.querySelector('body');
+  var backTop = document.querySelector('.back-to--Top');
+
+  backTop.addEventListener('click', function(){
+    ButteryScroll(body, 0, 500);
+  });
+
+  <!-- // https://gist.github.com/viljamis/6cd87add10414563acd9 -->
+  function ButteryScroll(scrollable, distance, duration) {
+    var startTime;
+    var startPos = scrollable.scrollTop;
+    var maxScroll = scrollable.scrollHeight - scrollable.offsetHeight;
+    var scrollEndValue = startPos + distance < maxScroll ? distance : maxScroll - startPos;
+
+    function easeInOutCubic(t, b, c, d) {
+      if ((t /= d / 2) < 1) {
+        return c / 2 * t * t * t + b;
+      }
+      return c / 2 * ((t -= 2) * t * t + 2) + b;
+    }
+
+    function scroll(timestamp) {
+      startTime = startTime || timestamp;
+      var elapsed = timestamp - startTime;
+      var progress = easeInOutCubic(elapsed, startPos, scrollEndValue, duration);
+      scrollable.scrollTop = progress;
+
+      if (elapsed < duration) {
+        requestAnimationFrame(scroll);
+      }
+    }
+    requestAnimationFrame(scroll);
+  }
+}
+
 function init() {
   Menu();
+  backToTop();
   hero();
 }
 
