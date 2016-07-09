cfd318b7968a785fdad4bfadf3dc25c8aa63e089
Jacques Uys <jaxuys@gmail.com>
2016-07-09 11:13:12 +0200

    added init & hero function

--- public/js/main.js
+++ public/js/main.js
@@ -2,27 +2,48 @@
 // https://github.com/GoogleChrome/ui-element-samples/blob/gh-pages/side-nav/side-nav.js
 -->
 
-var showButtonEl = document.querySelector('.menu-show');
-var sideNavEl = document.querySelector('.side-nav');
-var hideButtonEl = document.querySelector('.menu-hide');
+function Menu() {
+  var showButtonEl = document.querySelector('.menu-show');
+  var sideNavEl = document.querySelector('.side-nav');
+  var hideButtonEl = document.querySelector('.menu-hide');
 
-showButtonEl.addEventListener('click', showSideNav);
-hideButtonEl.addEventListener('click', hideSideNav);
-sideNavEl.addEventListener('click', hideSideNav);
+  showButtonEl.addEventListener('click', showSideNav);
+  hideButtonEl.addEventListener('click', hideSideNav);
+  sideNavEl.addEventListener('click', hideSideNav);
 
-function onTransitionEnd (evt) {
-  sideNavEl.classList.remove('side-nav--animatable');
-  sideNavEl.removeEventListener('transitionend', onTransitionEnd);
+  function onTransitionEnd (evt) {
+    sideNavEl.classList.remove('side-nav--animatable');
+    sideNavEl.removeEventListener('transitionend', onTransitionEnd);
+  }
+
+  function showSideNav() {
+    sideNavEl.classList.add('side-nav--animatable');
+    sideNavEl.classList.add('side-nav--visible');
+    sideNavEl.addEventListener('transitionend', onTransitionEnd);
+  }
+
+  function hideSideNav () {
+    sideNavEl.classList.add('side-nav--animatable');
+    sideNavEl.classList.remove('side-nav--visible');
+    sideNavEl.addEventListener('transitionend', onTransitionEnd);
+  }
 }
 
-function showSideNav() {
-  sideNavEl.classList.add('side-nav--animatable');
-  sideNavEl.classList.add('side-nav--visible');
-  sideNavEl.addEventListener('transitionend', onTransitionEnd);
+function hero() {
+  document.querySelector('body').classList.add('hero--visible');
 }
 
-function hideSideNav () {
-  sideNavEl.classList.add('side-nav--animatable');
-  sideNavEl.classList.remove('side-nav--visible');
-  sideNavEl.addEventListener('transitionend', onTransitionEnd);
+function init() {
+  Menu();
+  hero();
 }
+
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(init);
